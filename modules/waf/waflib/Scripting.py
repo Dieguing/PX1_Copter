3015763204600a093166c1769478db2dd58783bb
Diego Fernandez <ingdnfernandez@gmail.com>
2025-01-08 11:54:04 +0100

    Restoring original modules

--- modules/waf/waflib/Scripting.py
+++ modules/waf/waflib/Scripting.py
@@ -154,7 +154,6 @@ def waf_entry_point(current_directory, version, wafdir):
 		p = pstats.Stats('profi.txt')
 		p.sort_stats('time').print_stats(75) # or 'cumulative'
 	else:
-		# run_commands()
 		try:
 			try:
 				run_commands()
